82f7a2e3bb3b718312efbbd2897d3a380d3d218b
guoxc <qazwsxedc121@gmail.com>
2015-04-19 00:01:09 +0800

    solved p1062

--- src/p1062.java
+++ src/p1062.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by Administrator on 2015/4/18.
@@ -17,39 +14,33 @@ public class p1062 {
             parents.put(b,a);
         }
         int m = in.nextInt();
-        for(int i = 0; i < m; i += 1) {
+        FORLOOP:for(int i = 0; i < m; i += 1) {
             String x0 = in.next();
             String x1 = in.next();
-            ArrayList<String> list0 = new ArrayList<String>();
-            ArrayList<String> list1 = new ArrayList<String>();
-            while(parents.containsKey(x0)){
-                list0.add(x0);
+            HashSet<String> set0 = new HashSet<String>();
+            if(x0.equals(x1)){
+                System.out.println(x1);
+                continue;
+            }
+            while (parents.containsKey(x0)) {
+                set0.add(x0);
                 x0 = parents.get(x0);
-            };
-            while(parents.containsKey(x1)){
-                list1.add(x1);
-                x1 = parents.get(x1);
-            };
-            String res = findParents(list0, list1);
-            if(res != null ){
-                System.out.println(res);
-            }else {
-                System.out.println(-1);
             }
-        }
-
-
-    }
-    private static String findParents(List<String> l0, List<String> l1){
-        for(int i = 0; i < l0.size(); i += 1){
-            String a = l0.get(i);
-            for(int j = 0; j < l1.size(); j += 1){
-                String b = l1.get(j);
-                if(a.equals(b)){
-                    return a;
+            set0.add(x0);
+            while (parents.containsKey(x1)) {
+                if (set0.contains(x1)) {
+                    System.out.println(x1);
+                    continue FORLOOP;
                 }
+                x1 = parents.get(x1);
+
+            }
+            if (!set0.contains(x1)) {
+                System.out.println(-1);
+            }else {
+                System.out.println(x1);
             }
         }
-        return null;
     }
+
 }
